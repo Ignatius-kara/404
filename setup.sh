617ddb27a3dd53aa15c000803dcb0cc22d11c00f
Ignatius-kara <inyangignatius06@gmail.com>
2025-05-30 14:09:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-pip install --upgrade pip
-pip install torch==2.4.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip install -r requirements.txt
+pip install --upgrade pip wheel
+uv pip install torch==[2.4.1+cpu] -f https://github.com/pytorch/pytorch
+uv pip install plotly==[5.15.0] pip install matplotlib==3.9.2
+uv pip install -r requirements.txt
